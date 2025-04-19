346dc91f75f9e35213196e621ab09dc2f3f8182f
teucer12 <nathan.haleen@procellis.com>
2025-04-19 10:05:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def main(request):
 
     # Check for matching records
     for record in records:
-        if record.name == host and record.record_type == 'A' and ipv4:
+        if record.name == cfg.gcpDnsDomain and record.record_type == 'A' and ipv4:
             a_record_found = True
             for data in record.rrdatas:
                 if test_for_record_change(data, ipv4):
